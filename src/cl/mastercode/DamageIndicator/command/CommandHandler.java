a238ba9c5bf9c129a60749a2ea3a77f22d6f51bb
Beelzebu <jsuarez.lara@outlook.com>
2018-07-05 14:08:15 -0400

    Added configurable messages for /di toggle

--- src/cl/mastercode/DamageIndicator/command/CommandHandler.java
+++ src/cl/mastercode/DamageIndicator/command/CommandHandler.java
@@ -23,16 +23,51 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.ArmorStand;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 
 /**
- *
  * @author YitanTribal
  */
 @RequiredArgsConstructor
 public final class CommandHandler implements CommandExecutor {
 
     private final DIMain plugin;
+
+    private boolean isPlayer(CommandSender sender) {
+        if (sender instanceof Player) {
+            return true;
+        }
+        sender.sendMessage(ChatColor.RED + "Only a Player can use this command");
+        return false;
+    }
+
+    private boolean checkPermissions(CommandSender sender, String permission) {
+        if (sender.hasPermission(permission)) {
+            return true;
+        }
+        sender.sendMessage(ChatColor.RED + "You don't have permission to use this command this!");
+        return false;
+    }
+
+    private boolean checkArguments(CommandSender sender, int args, int num) {
+        if (args != num) {
+            sender.sendMessage(ChatColor.RED + "Invalid number of arguments");
+            return false;
+        }
+        return true;
+    }
+
+    private int getInt(CommandSender sender, String text) {
+        int amount;
+        try {
+            amount = Integer.parseInt(text);
+            return amount;
+        } catch (NumberFormatException e) {
+            sender.sendMessage(ChatColor.RED + "Invalid given amount");
+            return -1;
+        }
+    }
 
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String string, String[] strings) {
@@ -48,10 +83,7 @@ public final class CommandHandler implements CommandExecutor {
                     int range = getInt(sender, strings[1]);
                     if (range >= 0) {
                         int c = 0;
-                        c = ((Player) sender).getNearbyEntities(range, range, range).stream().filter((entity) -> (entity instanceof ArmorStand && plugin.isDamageIndicator((ArmorStand) entity, false))).map((entity) -> {
-                            entity.remove();
-                            return entity;
-                        }).map((_item) -> 1).reduce(c, Integer::sum);
+                        c = ((Player) sender).getNearbyEntities(range, range, range).stream().filter(entity -> entity instanceof ArmorStand && plugin.isDamageIndicator(entity, false)).peek(Entity::remove).map(_i -> 1).reduce(c, Integer::sum);
                         sender.sendMessage(ChatColor.GREEN + "" + c + " Damage Indicators were removed");
                         return true;
                     }
@@ -59,10 +91,7 @@ public final class CommandHandler implements CommandExecutor {
             } else if ("clearall".equalsIgnoreCase(strings[0])) {
                 if (isPlayer(sender) && (checkPermissions(sender, "damageindicator.admin"))) {
                     int c = 0;
-                    c = ((Player) sender).getLocation().getWorld().getEntitiesByClass(org.bukkit.entity.ArmorStand.class).stream().filter((as) -> (plugin.isDamageIndicator(as))).map((as) -> {
-                        as.remove();
-                        return as;
-                    }).map((_item) -> 1).reduce(c, Integer::sum);
+                    c = ((Player) sender).getLocation().getWorld().getEntitiesByClass(ArmorStand.class).stream().filter(plugin::isDamageIndicator).peek(Entity::remove).map(_i -> 1).reduce(c, Integer::sum);
                     sender.sendMessage(ChatColor.GREEN + "" + c + " Damage Indicators were removed in " + ((Player) sender).getLocation().getWorld().getName());
                     return true;
                 }
@@ -70,48 +99,9 @@ public final class CommandHandler implements CommandExecutor {
                 if (isPlayer(sender)) {
                     boolean status = !plugin.getStorageProvider().showArmorStand((Player) sender);
                     plugin.getStorageProvider().setShowArmorStand((Player) sender, status);
-                    sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&7Your damage indicator show status has changed to: " + (status ? "&aenabled" : "&cdisabled")));
+                    sender.sendMessage(ChatColor.translateAlternateColorCodes('&', status ? plugin.getConfig().getString("Messages.Damage Indicator.Enabled", "") : plugin.getConfig().getString("Messages.Damage Indicator.Disabled", "")));
                 } else {
                     sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&cThis command can't be executed from console."));
-                }
-            } else if ("debug".equalsIgnoreCase(strings[0])) {
-                if (isPlayer(sender) && (checkPermissions(sender, "damageindicator.admin")) && checkArguments(sender, strings.length, 2)) {
-                    int range = getInt(sender, strings[1]);
-                    if (range >= 0) {
-                        ((Player) sender).getNearbyEntities(range, range, range).stream().filter((entity) -> (entity instanceof ArmorStand)).map((entity) -> (ArmorStand) entity).map((as) -> {
-                            sender.sendMessage("Dysplay Name: " + as.getCustomName());
-                            return as;
-                        }).map((as) -> {
-                            sender.sendMessage("isSmall (true) " + as.isSmall());
-                            return as;
-                        }).map((as) -> {
-                            sender.sendMessage("isCustomNameVisible (true) " + as.isCustomNameVisible());
-                            return as;
-                        }).map((as) -> {
-                            sender.sendMessage("hasGravity (false) " + as.hasGravity());
-                            return as;
-                        }).map((as) -> {
-                            try {
-                                sender.sendMessage("isCollidable (false) " + as.isCollidable());
-                            } catch (Exception oldversion) {
-                            }
-                            return as;
-                        }).map((as) -> {
-                            sender.sendMessage("iisMarker (true) " + as.isMarker());
-                            return as;
-                        }).map((as) -> {
-                            sender.sendMessage("isVisible (false) " + as.isVisible());
-                            return as;
-                        }).map((as) -> {
-                            try {
-                                sender.sendMessage("isInvulnerable (true) " + as.isInvulnerable());
-                            } catch (Exception oldVersion) {
-                            }
-                            return as;
-                        }).forEachOrdered((as) -> {
-                            sender.sendMessage("getRemoveWhenFarAway (true) " + as.getRemoveWhenFarAway());
-                        });
-                    }
                 }
             }
         } else {
@@ -122,41 +112,6 @@ public final class CommandHandler implements CommandExecutor {
             sender.sendMessage(ChatColor.DARK_AQUA + "/di toggle " + ChatColor.AQUA + "#enable/disable damage indicators for you");
         }
         return true;
-    }
-
-    public static boolean isPlayer(CommandSender sender) {
-        if (sender instanceof Player) {
-            return true;
-        }
-        sender.sendMessage(ChatColor.RED + "Only a Player can use this command");
-        return false;
-    }
-
-    public static boolean checkPermissions(CommandSender sender, String permission) {
-        if (sender.hasPermission(permission)) {
-            return true;
-        }
-        sender.sendMessage(ChatColor.RED + "You don't have permission to use this command this!");
-        return false;
-    }
-
-    public static boolean checkArguments(CommandSender sender, int args, int num) {
-        if (args != num) {
-            sender.sendMessage(ChatColor.RED + "Invalid number of arguments");
-            return false;
-        }
-        return true;
-    }
-
-    public static int getInt(CommandSender sender, String text) {
-        int amount;
-        try {
-            amount = Integer.parseInt(text);
-            return amount;
-        } catch (NumberFormatException e) {
-            sender.sendMessage(ChatColor.RED + "Invalid given amount");
-            return -1;
-        }
     }
 
 }
